c59e76fabe579719ec7dc9b67930fe60657b24d9
Gabriel Jose Bazan <gbazan@outlook.com>
2018-03-16 20:37:15 -0300

    Improveme gateway

--- gateway.py
+++ gateway.py
@@ -1,5 +1,5 @@
 from os import listdir, makedirs
-from os.path import join, basename, isfile, exists
+from os.path import join, basename, isfile, exists, splitext
 import sys
 from importlib import import_module
 import inspect
@@ -26,6 +26,7 @@ class ProcessesGateway(object):
         self._create_local_directory(local_process_dir)
         with self.connection:
             self.connection.download_directory_contents(remote_process_dir, local_process_dir)
+        self._create_ini_file(local_process_dir)
         return local_process_dir
 
     def remove(self, identifier):
@@ -35,13 +36,16 @@ class ProcessesGateway(object):
     def get_instance(self, identifier, local_path):
         local_process_dir = self.get(identifier, local_path)
 
-        module_name, full_path = self.get_local_process_file(local_process_dir)
-        sys.path.insert(0, full_path)
+        module_name = self.get_local_process_file(local_process_dir)
+
+        sys.path.insert(0, local_process_dir)
         import_module(module_name)
+
+        class_name = self.to_camel_case(module_name)
 
         instance = None
         for name, _class in inspect.getmembers(sys.modules[module_name], inspect.isclass):
-            if _class.__name__ == identifier:
+            if _class.__name__ == class_name:
                 instance = _class()
 
         return instance
@@ -52,8 +56,11 @@ class ProcessesGateway(object):
 
     @staticmethod
     def get_local_process_file(local_process_dir):
-        process_dir_files = [f for f in listdir(local_process_dir) if isfile(join(local_process_dir, f))]
-        return process_dir_files[0], join(local_process_dir, process_dir_files[0])
+        process_file = '.'
+        for f in listdir(local_process_dir):
+            if isfile(join(local_process_dir, f)) and f != '__ini__.py':
+                process_file = f
+        return splitext(process_file)[0]
 
     def _get_remote_process_directory(self, identifier):
         return join(self.remote_directory, identifier)
@@ -62,3 +69,6 @@ class ProcessesGateway(object):
     def _create_local_directory(directory):
         if not exists(directory):
             makedirs(directory)
+
+    def _create_ini_file(self, directory):
+        open(join(directory, '__ini__.py'), 'a').close()
